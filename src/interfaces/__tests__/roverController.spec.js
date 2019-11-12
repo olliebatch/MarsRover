765a3f058e83d611799ca29be7ce9736553b479e
Ollie Batchelor <oliverb@nandos.co.uk>
2019-11-12 17:04:50 +0000

    RoverController test

---
+++ src/interfaces/__tests__/roverController.spec.js
@@ -0,0 +1,38 @@
+const roverController = require('../roverController');
+
+describe('Testing of the movement loops', () => {
+  it('Test output of movement loop', async () => {
+    const roverInfo = [
+      {
+        roverNo: '1',
+        xGrid: 1,
+        yGrid: 2,
+        direction: 'N',
+        movements: 'LMLMLMLMM',
+      },
+      {
+        roverNo: '2',
+        xGrid: 3,
+        yGrid: 3,
+        direction: 'E',
+        movements: 'MMRMMRMRRM',
+      },
+    ];
+    const gridSize = { x: 5, y: 5 };
+
+    const expectedRoverOutput = [
+      {
+        xGrid: 1,
+        yGrid: 3,
+        direction: 'N',
+      },
+      {
+        xGrid: 5,
+        yGrid: 1,
+        direction: 'E',
+      },
+    ];
+    const x = roverController(roverInfo, gridSize)
+    expect(x).toStrictEqual(expectedRoverOutput);
+  });
+});

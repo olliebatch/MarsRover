df90d71a3a63302d40d863f4bcf1bb9c282fe3b1
Ollie Batchelor <oliverb@nandos.co.uk>
2019-11-13 17:30:39 +0000

    roverController addition tests to check errors

--- src/interfaces/__tests__/roverController.spec.js
+++ src/interfaces/__tests__/roverController.spec.js
@@ -37,4 +37,48 @@ describe('Testing of the movement loops', () => {
     const x = await roverController(roverInfo, gridSize);
     expect(x).toStrictEqual(expectedRoverOutput);
   });
+  it('Test output of movement loop', async () => {
+    const roverInfo = [
+      {
+        roverNo: '1',
+        xGrid: 1,
+        yGrid: 2,
+        direction: 'N',
+        movements: 'LMMMMMM',
+      },
+      {
+        roverNo: '2',
+        xGrid: 3,
+        yGrid: 3,
+        direction: 'E',
+        movements: 'MMRMMRMRRM',
+      },
+    ];
+    const gridSize = { x: 5, y: 5 };
+
+    const expectedRoverOutput = [
+      {
+        roverNo: '1',
+        xGrid: 1,
+        yGrid: 3,
+        direction: 'N',
+      },
+      {
+        roverNo: '2',
+        xGrid: 5,
+        yGrid: 1,
+        direction: 'E',
+      },
+    ];
+    const x = await roverController(roverInfo, gridSize);
+    expect(x).toStrictEqual([
+      { roverNo: '1', error: 'Rover would fall off the specified x axis' },
+      {
+        roverNo: '2',
+        xGrid: 5,
+        yGrid: 1,
+        direction: 'E',
+      },
+    ]);
+  });
 });
